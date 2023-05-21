ee955fd15e7db47715f9a1b0a08284248dc2b152
gkundan <beingkundan188@gmail.com>
2023-05-21 08:20:39 +0530

    connect with mongodb

---
+++ Backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`MongoDb Server Connect With :${data.connection.host}  `);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = connectDatabase;
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/db");
 
 //config
 dotenv.config({ path: "backend/config/config.env" });
 
+//connect db
+connectDatabase();
 //
 app.listen(process.env.PORT, () => {
   console.log(`Server is Running on the port:${process.env.PORT}`);
