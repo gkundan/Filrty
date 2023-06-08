b4f565adbaeebc0746bfab476e28a6a4a4353777
gkundan <beingkundan188@gmail.com>
2023-06-08 08:56:53 +0530

    complete the authentication

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -4,6 +4,8 @@ const catchAsyncError = require("../middleware/catchAsyncError");
 //model
 const User = require("../models/UserModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 //register a user
 exports.registerUser = catchAsyncError(async (req, res, next) => {
@@ -58,4 +60,74 @@ exports.logout = catchAsyncError(async (req, res, next) => {
     success: true,
     message: "Logged Out !",
   });
+});
+
+//forgot password
+exports.forgotPassword = catchAsyncError(async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      throw new ErrorHandler("User Not Found", 404);
+    }
+
+    //get resetPassword Token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    const resetPasswordUrl = `${req.protocol}://${req.get(
+      "host"
+    )}/api/v1/password/reset/${resetToken}`;
+
+    const message = `Your Password reset token is: ${resetPasswordUrl}. If you have not requested this email, please ignore it. Thank you.`;
+
+    console.log(req.body);
+
+    await sendEmail({
+      email: user.email,
+      subject: `Flirty Password Recovery`,
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email Sent to ${user.email} successfully!`,
+    });
+  } catch (error) {
+    next(error); // Pass the error to the error handling middleware
+  }
+});
+
+//reset password
+exports.resetPassword = catchAsyncError(async (req, res, next) => {
+  //creating has token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  //find user from token
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler("Reset Password Token is invalid or expire ", 400)
+    );
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Passowrd doesn't Password", 400));
+  }
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendToken(user, 200, res);
 });
--- Backend/routes/UserRoute.js
+++ Backend/routes/UserRoute.js
@@ -3,13 +3,21 @@ const {
   registerUser,
   logInUser,
   logout,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/UserController");
 const router = express.Router();
 
-//
+//register
 router.route("/register").post(registerUser);
-//
+
+//login
 router.route("/login").post(logInUser);
-//
+
+//forgot password
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+//logout
 router.route("/logout").get(logout);
+
 module.exports = router;
